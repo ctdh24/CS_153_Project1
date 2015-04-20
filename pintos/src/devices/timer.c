ab05cc912e929f72c316a11589dbb3973f9d8736
Jeffrey C <chen.jeffrey.r@gmail.com>
2015-04-20 15:32:09 -0700

    add TODO messages in timer_sleep(), more notes on timer_interrupt()

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -92,20 +92,27 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  int64_t start = timer_ticks ();
+  // get timer tick amount
+  int64_t start = timer_ticks();
   // get thread
   struct thread *t = thread_current();
-  // figure out wake timer (timer_ticks())
   // 	awakeTime;
 
   ASSERT (intr_get_level () == INTR_ON);
   // disable interrupts to block thread
-  curState = intr_disable() 
-  // add thread t to a list of sleeping threads
-  // 	
-  // can order list by awakeTime
-  // put thread to sleep -> Block()
+  enum intr_level old_level = intr_disable ();
+  // TODO: implement a wakeup tick variable to each thread
+  // set t's wakeup tick
+  // t->wakeup_time = start + ticks;
+
+  // TODO: add thread t to a list of sleeping threads
+  // order list by wakeup tick
+
+  // block thread until woken
   t.thread_block();
+  // enable interrupts
+  intr_set_level(old_level);
+  // OLD CODE
   //while (timer_elapsed (start) < ticks) 
   // thread_yield ();
 
@@ -180,17 +187,17 @@ timer_print_stats (void)
 {
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
-
+
 /* Timer interrupt handler. */
 static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
-  thread_tick ();
-  // look through list
+  thread_tick();
+  // look through blocked thread list
+  // assuming list is sorted, check first thread
   // compare thread t awakeTime to ticks
-  // if time to awake, unblock() and remove from sleeping list
-  // PART B - run thread if highest priority
+  // if time to awake, unblock() and move from sleeping list to ready list
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
