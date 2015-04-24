bf7c4b1a9b0f7ca8265e199742c8c4e4dd7a11c9
Jeffrey C <chen.jeffrey.r@gmail.com>
2015-04-23 17:58:22 -0700

    added function prototype in thread.h for comparator function

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -141,4 +141,8 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+bool cmp_ticks (const struct list_elem *a,
+    const struct list_elem *b,
+    void *aux UNUSED);
+
 #endif /* threads/thread.h */
